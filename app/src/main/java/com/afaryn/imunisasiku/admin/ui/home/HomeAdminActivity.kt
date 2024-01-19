67546e1ea46c5e85ab21b4d6193413d4dbccdcf0
afaryn <audy.fitri10@gmail.com>
2024-01-19 23:01:38 +0700

    feat:Dashboard sycn

--- app/src/main/java/com/afaryn/imunisasiku/admin/ui/home/HomeAdminActivity.kt
+++ app/src/main/java/com/afaryn/imunisasiku/admin/ui/home/HomeAdminActivity.kt
@@ -2,18 +2,47 @@ package com.afaryn.imunisasiku.admin.ui.home
 
 
 import android.content.Intent
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import com.afaryn.imunisasiku.R
 import com.afaryn.imunisasiku.admin.ui.jadwal.TambahJadwal
 import com.afaryn.imunisasiku.admin.ui.profile.ProfileFragmentAdmin
 import com.afaryn.imunisasiku.databinding.HomeAdminBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
 
 class HomeAdminActivity : AppCompatActivity() {
     private lateinit var binding: HomeAdminBinding
     private lateinit var navController: NavController
+
+    private var isExpanded = false
+
+    private val fromBottomFabAnim: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.from_bottom_fab)
+    }
+    private val toBottomFabAnim: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.to_bottom_fab)
+    }
+    private val rotateClockWiseFabAnim: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.rotate_clock_wise)
+    }
+    private val rotateAntiClockWiseFabAnim: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.rotate_anti_clock_wise)
+    }
+    private val fromBottomBgAnim: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim)
+    }
+    private val toBottomBgAnim: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= HomeAdminBinding.inflate(layoutInflater)
@@ -34,9 +63,24 @@ class HomeAdminActivity : AppCompatActivity() {
                 }
                 true
             }
+//            btnAdd.setOnClickListener {
+//                val intent = Intent(this@HomeAdminActivity,TambahJadwal::class.java)
+//                startActivity(intent)
+//            }
             btnAdd.setOnClickListener {
-                val intent = Intent(this@HomeAdminActivity,TambahJadwal::class.java)
-                startActivity(intent)
+
+                if (isExpanded) {
+                    shrinkFab()
+                } else {
+                    expandFab()
+                }
+
+            }
+            btnHari.setOnClickListener {
+                Toast.makeText(this@HomeAdminActivity,"btn Hari Clicked",Toast.LENGTH_SHORT).show()
+            }
+            btnJam.setOnClickListener {
+                Toast.makeText(this@HomeAdminActivity,"btn Jam Clicked",Toast.LENGTH_SHORT).show()
             }
         }
     }
@@ -48,6 +92,59 @@ class HomeAdminActivity : AppCompatActivity() {
         fragmentTransaction.commit()
     }
 
+    private fun shrinkFab() {
+
+        binding.transparentBg.startAnimation(toBottomBgAnim)
+        binding.btnAdd.startAnimation(rotateAntiClockWiseFabAnim)
+        binding.btnHari.startAnimation(toBottomFabAnim)
+        binding.btnJam.startAnimation(toBottomFabAnim)
+        binding.tvHari.startAnimation(toBottomFabAnim)
+        binding.shareTv.startAnimation(toBottomFabAnim)
+
+
+
+        isExpanded = !isExpanded
+    }
+
+    private fun expandFab() {
+
+        binding.transparentBg.startAnimation(fromBottomBgAnim)
+
+
+        binding.btnAdd.startAnimation(rotateClockWiseFabAnim)
+        binding.btnHari.startAnimation(fromBottomFabAnim)
+        binding.btnJam.startAnimation(fromBottomFabAnim)
+        binding.tvHari.startAnimation(fromBottomFabAnim)
+        binding.shareTv.startAnimation(fromBottomFabAnim)
+
+
+        isExpanded = !isExpanded
+    }
+
+    override fun onBackPressed() {
+
+        if (isExpanded) {
+            shrinkFab()
+        } else {
+            super.onBackPressed()
+
+        }
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+
+        if (ev?.action == MotionEvent.ACTION_DOWN) {
+
+            if (isExpanded) {
+                val outRect = Rect()
+                binding.fabConstraint.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(ev.rawX.toInt(), ev.rawY.toInt())) {
+                    shrinkFab()
+                }
+            }
+        }
+        return super.dispatchTouchEvent(ev)
+    }
 
 
 }
--- app/src/main/java/com/afaryn/imunisasiku/admin/ui/home/HomeFragmentAdmin.kt
+++ app/src/main/java/com/afaryn/imunisasiku/admin/ui/home/HomeFragmentAdmin.kt
@@ -6,12 +6,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.afaryn.imunisasiku.admin.ui.home.viewModel.HomeAdminViewModel
 import com.afaryn.imunisasiku.admin.ui.kelolaAkun.KelolaAkun
 import com.afaryn.imunisasiku.admin.ui.kelolaImunisasi.KelolaImunisasi
 import com.afaryn.imunisasiku.databinding.FragmentHomeAdminBinding
+import com.afaryn.imunisasiku.utils.UiState
+import com.afaryn.imunisasiku.utils.hide
+import com.afaryn.imunisasiku.utils.show
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragmentAdmin : Fragment() {
     private lateinit var binding: FragmentHomeAdminBinding
+    private val viewModel by viewModels<HomeAdminViewModel>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,6 +30,7 @@ class HomeFragmentAdmin : Fragment() {
         val view = binding.root
 
         setAction()
+        setPage()
 
         return view
     }
@@ -35,4 +46,67 @@ class HomeFragmentAdmin : Fragment() {
             }
         }
     }
+
+    private fun setPage(){
+        binding.apply {
+            viewModel.getData()
+
+            viewModel.getState.observe(viewLifecycleOwner) { state ->
+                when (state) {
+                    is UiState.Loading -> {
+                        // Menampilkan progress bar
+                        progressBar.visibility = View.VISIBLE
+                    }
+                    is UiState.Success -> {
+                        // Menyembunyikan progress bar
+                        progressBar.visibility = View.GONE
+                        // Mendapatkan data sebagai map
+                        val data = state.data
+                        // Menampilkan data pada UI
+                        tvAkun.text = data!!["akun"]
+                        tvLayanan.text = data["imunisasi"]
+                        tvAntrian.text = data["pasien"]
+                        textView.text = "Selamat Datang, "+data["username"]
+                    }
+                    is UiState.Error -> {
+                        // Menyembunyikan progress bar
+                        progressBar.visibility = View.GONE
+                        // Menampilkan pesan error
+                        Toast.makeText(requireContext(), state.error, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+            // Memanggil fungsi untuk mengambil data
+
+
+
+        }
+
+
+    }
+
+//    private fun observe(){
+//        viewModel.getState.observe(this){
+//            when(it){
+//                is UiState.Loading ->{
+//                    if(it.isLoading==true)binding.progressBar.show()
+//                    else  binding.progressBar.hide()
+//                }
+//                is UiState.Success->{
+//                    val data = it.data
+//                    binding.apply {
+//                        tvAkun.text = data.
+//                    }
+////                    val intent = Intent(this,KelolaAkun::class.java)
+////                    startActivity(intent)
+//                }
+//                is UiState.Error->{
+//                    Toast.makeText(context,it.error!!, Toast.LENGTH_SHORT).show()
+//                }
+//            }
+//        }
+//    }
+
+
 }
